2345e2f6aeb334cf3f39f6b08bb8abc8849e84ca
Qirit0 <30974700+itz-8infinite@users.noreply.github.com>
2020-04-22 15:12:18 +0530

    Update muchmuchstrings.py

--- imports/muchmuchstrings.py
+++ imports/muchmuchstrings.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python
 
-    #Malware DB - the most awesome free malware database on the air
-    #Copyright (C) 2014, Yuval Nativ, Lahad Ludar, 5Fingers
-
-    #This program is free software: you can redistribute it and/or modify
-    #it under the terms of the GNU General Public License as published by
-    #the Free Software Foundation, either version 3 of the License, or
-    #(at your option) any later version.
-
-    #This program is distributed in the hope that it will be useful,
-    #but WITHOUT ANY WARRANTY; without even the implied warranty of
-    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    #GNU General Public License for more details.
-
-    #You should have received a copy of the GNU General Public License
-    #along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from imports import globals
 
 
